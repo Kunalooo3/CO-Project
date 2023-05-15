7f9553475c9420dfde926ce6b3ee5ea09058b6c3
harshrawat22202 <132182700+harshrawat22202@users.noreply.github.com>
2023-05-15 21:05:51 +0530

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -23,3 +23,24 @@ def check_variable_declaration_beginning(L1,f):
                 exit()
     D1={i:bin(D[i])[2:] for i in D.keys()}
     return variables,D1
+
+def check_label(L,f):
+    c=0        #line numbers                                   
+    labels=[]
+    D={}
+    for i in L:
+        if i[0]=='var':
+            continue
+        if i[0].count(":")==1:
+            if i[0][:-1] not in labels:
+                labels.append(i[0][:-1])
+                D[c]=i[0][:-1]
+            # else:
+            #     print("error in line",c)
+            #     raise SyntaxError("multiple definition for same label")
+        if (i[0].count(":")>1):
+            f.write("invalid label name")
+            exit()
+        c=c+1
+    D_new={D[i]:bin(i)[2:] for i in D.keys()}
+    return D_new,labels
