38ca0794ae331e47e34bf39fba1a0fdb35ae254a
harshrawat22202 <132182700+harshrawat22202@users.noreply.github.com>
2023-06-06 22:21:45 +0530

    Add files via upload
    added contents for simulator

---
+++ CO_A_P1_BASH/SimpleSimulator/typeB.py
@@ -0,0 +1,30 @@
+import Globals
+def movf(r1:str,Imm:str)->None:
+    if (len(Imm)>8):
+        Globals.registers['111']="0"*12+"1"+"0"*3
+        Globals.registers[r1]="0"*16
+        return 
+    Globals.registers[r1]=Imm.zfill(16)
+def movI(r1:str,Imm:str)-> None:
+    if (len(Imm)>7):
+        Globals.registers['111']="0"*12+"1"+"0"*3
+        Globals.registers[r1]="0"*16
+        return 
+    Globals.registers[r1]=Imm.zfill(16)
+def rs(r1:str,Imm: str)-> None:
+    T=int(Globals.registers[Imm],2)
+    shifted_value = int(r1) >> T
+    Globals.registers[r1]=bin(int(shifted_value))[2:].zfill(16)
+def ls(r1:str,Imm:str)-> None:
+    T=int(Globals.registers[Imm],2)
+    shifted_value = int(r1) << T
+    Globals.registers[r1]=bin(int(shifted_value))[2:].zfill(16)
+def type_B(instruction:str)->None:
+    if (instruction[0:5]=="00010"):
+        movI(instruction[6:9],instruction[9:])
+    if (instruction[0:5]=="10010"):
+        movf(instruction[5:8],instruction[8:])
+    if (instruction[0:5]=="01000"):
+        rs(instruction[6:9],instruction[9:])
+    if (instruction[0:5]=="01001"):
+        ls(instruction[6:9],instruction[9:])
